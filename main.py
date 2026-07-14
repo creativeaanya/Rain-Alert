221ad216966adb0c43d36b881597668906eeeda5
Paras Bansal <parasbansl22@gmail.com>
2026-07-14 18:13:37 +0530

    Add import for os module in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-
+import os
 from datetime import datetime
 import pandas
 import random
